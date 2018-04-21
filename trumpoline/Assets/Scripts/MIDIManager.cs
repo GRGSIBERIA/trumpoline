028ffdb6ee45472936c6686c155dcf45cf6e079f
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-04-22 00:28:06 +0900

    ノートオン，ノートオフに対応しつつ，細かい点を修正

--- trumpoline/Assets/Scripts/MIDIManager.cs
+++ trumpoline/Assets/Scripts/MIDIManager.cs
@@ -8,21 +8,43 @@ public class CorrectTrackException : System.Exception
 
 }
 
-public struct Note
+public class Note
 {
-    byte noteNumber;
-    float time;
-    byte velocity;
+    public byte NoteNumber { get; private set; }    // ノート番号
+    public float StartTime { get; private set; }    // 吹き始め時間
+    public float EndTime { get; private set; }      // 吹き終わり時間
+    public byte Velocity { get; private set; }      // ベロシティ
+
+    public Note(byte notenum, float time, byte vel)
+    {
+        this.NoteNumber = notenum;
+        this.StartTime = time;
+        this.Velocity = vel;
+        this.EndTime = time + 1;
+    }
+
+    public void SetEndTime(float time)
+    {
+        this.EndTime = time;
+    }
 }
 
 public class MIDIManager
 {
     public float Tempo { get; private set; } = 120f;
-    public List<float> EventSeconds { get; private set; }
+    public List<Note> Events { get; private set; }
 
-    MIDIManager()
+    public MIDIManager(byte[] midiBytes)
     {
-        
+        var smf = MidiFileLoader.Load(midiBytes);
+
+        // トラック0にテンポ，トラック1にノーツが入っている前提
+        // テンポとトランペットのトラック以外が入っていたら落とす
+        if (smf.tracks.Count == 2)
+            throw new CorrectTrackException();
+
+        this.Tempo = LoadTempo(smf);
+        this.Events = LoadEvents(smf, this.Tempo);
     }
 
     float LoadTempo(MidiFileContainer smf)
@@ -41,35 +63,39 @@ public class MIDIManager
         return tmp;
     }
 
-    List<float> LoadEvents(MidiFileContainer smf, float tmp)
+    float getTotalSeconds(int totalDelta, int division, float tmp)
+    {
+        return ((float)totalDelta / division) * (60f / tmp);
+    }
+
+    List<Note> LoadEvents(MidiFileContainer smf, float tmp)
     {
         var track = smf.tracks[1];
-        var events = new List<float>();
+        var events = new List<Note>();
         var totalDelta = 0;
-        
+
+
         foreach (var ev in track)
         {
-            if ((ev.midiEvent.status & 0xf0) == 0x80)
+            // 0x09がノートオン，0x08がノートオフになっている
+            // 他のサイトで逆になっているケースを見かけた
+
+            if ((ev.midiEvent.status & 0xf0) == 0x90)   // ノートオン
             {
                 var totalSeconds = ((float)totalDelta / smf.division) * (60f / tmp);
-                events.Add(totalSeconds);
+                events.Add(new Note(
+                    ev.midiEvent.data1,
+                    getTotalSeconds(totalDelta, smf.division, tmp),
+                    ev.midiEvent.data2));
+            }
+            else if ((ev.midiEvent.status & 0xf0) == 0x80)  // ノートオフ
+            {
+                var totalSec = getTotalSeconds(totalDelta, smf.division, tmp);
+                events[events.Count - 1].SetEndTime(totalSec);
             }
             totalDelta += ev.delta;
         }
 
         return events;
-    }
-
-    void InitializeMIDI(byte[] midiBytes)
-    {
-        var smf = MidiFileLoader.Load(midiBytes);
-
-        // トラック0にテンポ，トラック1にノーツが入っている前提
-        // テンポとトランペットのトラック以外が入っていたら落とす
-        if (smf.tracks.Count == 2)
-            throw new CorrectTrackException();
-
-        this.Tempo = LoadTempo(smf);
-        this.EventSeconds = LoadEvents(smf, this.Tempo);
     }
 }

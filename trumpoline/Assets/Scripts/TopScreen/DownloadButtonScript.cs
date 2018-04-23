f10d8493f6e8294ebf2c4473fa1ebacbe4cc3830
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-04-23 15:08:50 +0900

    set download button

--- trumpoline/Assets/Scripts/TopScreen/DownloadButtonScript.cs
+++ trumpoline/Assets/Scripts/TopScreen/DownloadButtonScript.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DownloadButtonScript : MonoBehaviour {
 
-    public GameObject InputField;
+    public GameObject InputFieldObj;
+    public string URL { get; private set; }
 
+    InputField inputField;
+    
 	// Use this for initialization
 	void Start () {
-		
+        this.inputField = InputFieldObj.GetComponent<InputField>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void OnClick()
+    {
+        this.URL = this.inputField.text;
+    }
 }

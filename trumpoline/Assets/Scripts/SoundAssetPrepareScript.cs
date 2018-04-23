3525c49b7e2b9f4f31e0b4416b7090967e7c25e7
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-04-23 15:24:08 +0900

    add sound asset manager class

---
+++ trumpoline/Assets/Scripts/SoundAssetPrepareScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundAssetDownloader : MonoBehaviour {
+
+    public GameObject MIDIDLButton;
+    public GameObject WAVDLButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

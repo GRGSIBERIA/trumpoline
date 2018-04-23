07345a11380829cce2ab8f13830930f3f9a9fb83
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-04-23 15:22:35 +0900

    add objects

--- trumpoline/Assets/Scripts/GameMaster.cs
+++ trumpoline/Assets/Scripts/GameMaster.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
+    public GameObject MIDIDL;
+    public GameObject WAVDL;
+
 	// Use this for initialization
 	void Start () {
 		

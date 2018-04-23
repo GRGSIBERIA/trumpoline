fd64c97ab9e707688432a5c4653803743240c905
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-04-23 17:21:44 +0900

    add download process

--- trumpoline/Assets/Scripts/TopScreen/DownloadButtonScript.cs
+++ trumpoline/Assets/Scripts/TopScreen/DownloadButtonScript.cs
@@ -1,37 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEngine.UI;
+
+
+public enum FileType
+{
+    MIDI,
+    WAV
+}
 
 public class DownloadButtonScript : MonoBehaviour {
 
     public GameObject InputFieldObj;
     public string URL { get; private set; }
+    public bool downloadEnded { get; private set; }
+
+    public AudioClip clip;
+    public byte[] midi;
+
+    public FileType AudioType { get; private set; }
 
     InputField inputField;
     WWW www;
-    
+
+    void Awake()
+    {
+        if (this.name.IndexOf("WAV") >= 0)
+        {
+            this.AudioType = FileType.WAV;
+        }
+        else if (this.name.IndexOf("MIDI") >= 0)
+        {
+            this.AudioType = FileType.MIDI;
+        }
+        else
+        {
+            throw new System.Exception("Do not match names: MIDI or WAV");
+        }
+    }
+
+
 	// Use this for initialization
 	void Start () {
         this.inputField = InputFieldObj.GetComponent<InputField>();
-        
-        // これは失敗する気がする
-        if (www != null && www.isDone)
-        {
-
-        }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+    IEnumerator GetContents()
+    {
+        yield return new WaitWhile(() => this.www.isDone);
+    }
+
+    // MIDIやWAVのバリデーションをしたい
+
+
     public void OnClick()
     {
         this.URL = this.inputField.text;
-        
-        www = new WWW(this.URL);
+
+        this.www = new WWW(this.URL);
+        while (!this.www.isDone) { }
+
+        this.downloadEnded = this.www.isDone;
+        Debug.Log(this.downloadEnded);
+
+        switch (AudioType)
+        {
+            case FileType.MIDI:
+                this.midi = this.www.bytes;
+                Debug.Log(this.midi.Length);
+                break;
+            case FileType.WAV:
+                this.clip = this.www.GetAudioClip(false, true);
+                break;
+        }
     }
 }
---
+++ trumpoline/Assets/Scripts/TopScreen/SuccessImageScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuccessImageScript : MonoBehaviour {
+
+    public GameObject DLButton;
+
+    DownloadButtonScript dlButton;
+    Image image;
+    
+    Color SetColor(float a)
+    {
+        var inic = this.image.color;
+        inic.a = a;
+        return inic;
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.dlButton = this.DLButton.GetComponent<DownloadButtonScript>();
+        this.image = this.GetComponent<Image>();
+
+        this.image.color = SetColor(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (dlButton.downloadEnded)
+        {
+            this.image.color = SetColor(1);
+        }
+	}
+}

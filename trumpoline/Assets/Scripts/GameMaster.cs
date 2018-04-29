3fbb891c2540363dd2d4b05168b82d4b2aa72964
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-04-29 18:45:58 +0900

    add register component for transfar scene

--- trumpoline/Assets/Scripts/GameMaster.cs
+++ trumpoline/Assets/Scripts/GameMaster.cs
@@ -2,23 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : MonoBehaviour {
+public class GameMaster : ScriptableObject {
 
     public AudioClip clip;
     public byte[] midiData;
 
     MIDIManager midiManager;
-
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+    
     public void LoadMIDIManager()
     {
         this.midiManager = new MIDIManager(this.midiData);
---
+++ trumpoline/Assets/Scripts/SceneTransferScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(-100)]
+public class SceneTransferScript : MonoBehaviour {
+    [SerializeField]
+    UnityEngine.Events.UnityEvent onAwake;
+
+    void Awake()
+    {
+        this.onAwake.Invoke();
+    }
+}

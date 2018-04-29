ba3a2af7c629a0ceb5445230660dca94a44a3439
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-04-29 18:50:12 +0900

    move prefabs to game screen folder

--- trumpoline/Assets/Scripts/GameMaster.cs
+++ trumpoline/Assets/Scripts/GameMaster.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : ScriptableObject {
+public class GameMaster : MonoBehaviour {
 
     public AudioClip clip;
     public byte[] midiData;
